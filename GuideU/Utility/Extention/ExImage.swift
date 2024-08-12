7a50add5a84086d243c4d48227f7e629715ec117
Kim JaeHyung <you3192001@icloud.com>
2024-08-12 23:26:01 +0900

    Fetch: Image Setting

---
+++ GuideU/Utility/Extention/ExImage.swift
@@ -0,0 +1,54 @@
+//
+//  ExImage.swift
+//  GuideU
+//
+//  Created by Jae hyung Kim on 8/12/24.
+//
+
+import SwiftUI
+
+extension Image {
+    
+    static let appLogo = Image(.appLogo)
+    
+    enum TabbarImage {
+        enum noneTab {
+            static let homeTab = Image(.homeTab)
+            static let memeTab = Image(.memeTab)
+            static let historyTab = Image(.historyTab)
+            static let settingTab = Image(.settingTab)
+        }
+        
+        enum selected {
+            static let homeTab = Image(.homeSelectedTab)
+            static let memeTab = Image(.memeSeletedTab)
+            static let historyTab = Image(.historySelectedTab)
+            static let settingTab = Image(.settingSelectedTab)
+        }
+    }
+    
+    enum SplashImage {
+        static let splash = Image(.splash1)
+    }
+    
+    enum OnBoardImage: CaseIterable {
+        case first
+        case second
+        case third
+        case fore
+        
+        var img: Image {
+            switch self {
+            case .first:
+                return Image(.page1)
+            case .second:
+                return Image(.page2)
+            case .third:
+                return Image(.page3)
+            case .fore:
+                return Image(.page4)
+            }
+        }
+    }
+}
+
