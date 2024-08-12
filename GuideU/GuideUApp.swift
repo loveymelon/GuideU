20108231fd7294b01e4d036c34b69c653bad9226
Kim JaeHyung <you3192001@icloud.com>
2024-08-12 22:41:28 +0900

    git Inital

---
+++ GuideU/GuideUApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideUApp.swift
+//  GuideU
+//
+//  Created by Jae hyung Kim on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GuideUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
