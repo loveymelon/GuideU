7125f5a6e55159bd63314506b4b5ee14b20357a2
Kim JaeHyung <you3192001@icloud.com>
2024-08-20 13:59:22 +0900

    Update: 홈 화면 슬라이드시 작아지는 효과 구성

--- GuideU/Presentation/Tab/Home/HomeView.swift
+++ GuideU/Presentation/Tab/Home/HomeView.swift
@@ -12,51 +12,76 @@ struct HomeView: View {
     @State private var offsetY: CGFloat = 0
     
     var body: some View {
-        GeometryReader { geometry in
-            let size = geometry.size
-            let safeArea = geometry.safeAreaInsets
-            
-            ScrollView(.vertical, showsIndicators: false) {
-                VStack(spacing: 0) {
-                    headerView(size: size, safeArea: safeArea)
-                }
-                .zIndex(1000)
-                .background {
-                    ScrollDetector { offset in
-                        offsetY = offset
-                    } onDraggingEnd: { offset, velocity in
-                        
+        VStack {
+            GeometryReader {
+                let safeArea = $0.safeAreaInsets
+                
+                GeometryReader { geometry in
+                    let size = geometry.size
+                    let minY = geometry.frame(in: .named("SCROOL")).minY
+                    ScrollView(.vertical, showsIndicators: false) {
+                        VStack(spacing: 0) {
+                            headerView(size: size, safeArea: safeArea)
+                                .zIndex(1000)
+                            LazyVStack {
+                                ForEach(1...100, id: \.self) { num in
+                                    HStack {
+                                        Text(String(num))
+                                    }
+                                }
+                            }
+                            .frame(width: size.width)
+                        }
+                        .background {
+                            ScrollDetector { offset in
+                                offsetY = -offset
+                            } onDraggingEnd: { offset, veloc in
+                                
+                            }
+                        }
+                        .zIndex(1000)
                     }
+                    .toolbar(.hidden, for: .navigationBar)
                 }
+                .ignoresSafeArea(.all, edges: .top)
             }
-            .ignoresSafeArea(.all, edges: .top)
-            .toolbar(.hidden, for: .navigationBar)
         }
     }
     
     @ViewBuilder
     func headerView(size: CGSize, safeArea: EdgeInsets) -> some View {
         let headerHeight = (size.height * 0.26) + safeArea.top
+        let minHeight = 70 + safeArea.top
+        let changeHeight = (headerHeight + offsetY)
         
-        ZStack {
-            /// 배경 이미지
-            Image.defaultBack.resizable()
-                .aspectRatio(contentMode: .fill)
-                .blur(radius: 20, opaque: true)
-                .opacity(0.3)
-                .frame(height: headerHeight) // 배경의 높이 설정
+        GeometryReader { proxy in
+            let size = proxy.size
+                backgroundImage(size: size, minY: offsetY)
+                    .offset(y: -offsetY)
+                    .frame(height: changeHeight < minHeight ? minHeight : changeHeight, alignment: .bottom)
             
-            GeometryReader { _ in
-                fakeNavigation()
-                    .padding(.top, safeArea.top)
-                    .frame(width: size.width)
-            }
+                fakeNavigation(size: size)
+                    .padding(.top, safeArea.top + 10)
+                    .offset(y: -offsetY)
+            
         }
         .frame(height: headerHeight)
-        .offset(y: offsetY)
     }
     
-    private func fakeNavigation() -> some View {
+    private func backgroundImage(size: CGSize, minY: CGFloat) -> some View {
+        Group {
+            /// 조건에 따라 이미지 변경할것.
+            Image.defaultBack.resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: size.width, height: size.height + (minY > 0 ? minY : 0))
+                .clipped()
+        }
+        .blur(radius: 20, opaque: true)
+        .opacity(0.4)
+        .background(.white)
+    }
+    
+    private func fakeNavigation(size: CGSize)  -> some View {
         HStack {
             Image.appLogo
                 .resizable()
@@ -70,8 +95,7 @@ struct HomeView: View {
                 .aspectRatio(1, contentMode: .fit)
                 .frame(height: 32)
         }
-        .padding(.trailing, 10)
-        .padding(.leading, 10)
+        .padding(.horizontal, 10)
     }
 }
 
--- GuideU/Utility/Extention/ExView.swift
+++ GuideU/Utility/Extention/ExView.swift
@@ -11,4 +11,24 @@ extension View {
     func asButton(action: @escaping () -> Void ) -> some View {
         modifier(ButtonWrapper(action: action))
     }
+    
+    
+    func headerGradient() -> some View {
+        self
+            .overlay {
+                ZStack(alignment: .bottom) {
+                    Rectangle()
+                        .fill(
+                            .linearGradient(colors: [
+                                .black.opacity(0),
+                                .black.opacity(0.1),
+                                .black.opacity(0.3),
+                                .black.opacity(0.6),
+                                .black.opacity(0.8),
+                                .black.opacity(1)
+                            ], startPoint: .top, endPoint: .bottom)
+                        )
+                }
+            }
+    }
 }
