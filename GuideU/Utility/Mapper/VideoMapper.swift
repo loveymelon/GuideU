bcd2dc99127c277c595c0cb89f78858b5c6aaf29
loveyMelon <manciepmc@gmail.com>
2024-08-29 21:43:07 +0900

    Fetch MoreCharacter

---
+++ GuideU/Utility/Mapper/VideoMapper.swift
@@ -0,0 +1,8 @@
+//
+//  VideoMapper.swift
+//  GuideU
+//
+//  Created by 김진수 on 8/29/24.
+//
+
+import Foundation
---
+++ GuideU/Utility/Repository/VideoRepository.swift
@@ -0,0 +1,8 @@
+//
+//  VideoRepository.swift
+//  GuideU
+//
+//  Created by 김진수 on 8/29/24.
+//
+
+import Foundation
