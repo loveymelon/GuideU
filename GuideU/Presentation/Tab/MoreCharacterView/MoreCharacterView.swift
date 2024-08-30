7d9a244a4777b3bf3263ab5c0da9c0be271fd115
Kim JaeHyung <you3192001@icloud.com>
2024-08-31 01:33:39 +0900

    [Update] MoreCharacterView

--- GuideU/Presentation/Tab/MoreCharacterView/MoreCharacterView.swift
+++ GuideU/Presentation/Tab/MoreCharacterView/MoreCharacterView.swift
@@ -12,54 +12,97 @@ struct MoreCharacterView: View {
     
     @Perception.Bindable var store: StoreOf<MoreCharacterFeature>
     
+    @State private var isSearchVisible: Bool = true
+    @State private var scrollOffset: CGFloat = 0
+    @State private var lastScrollOffset: CGFloat = 0
+    
     var body: some View {
         WithPerceptionTracking{
-            contentView()
-                .onAppear {
-                    store.send(.viewCycleType(.onAppear))
+            ZStack(alignment: .top) {
+                GuideUSearchBarView(currentText: $store.currentText.sending(\.currentText), placeHolder: store.constViewState.placeHolder, lineWidth: 1.4) {
+                    store.send(.viewEventType(.onSubmit))
                 }
+                .padding(.horizontal, 10)
+                
+                contentView()
+                    .onAppear {
+                        store.send(.viewCycleType(.onAppear))
+                    }
+                    .padding(.top, isSearchVisible ? 54 : 0 )
+            }
         }
     }
 }
 
 extension MoreCharacterView {
     private func contentView() -> some View {
-        VStack {
-            GuideUSearchBarView(currentText: $store.currentText.sending(\.currentText), placeHolder: store.constViewState.placeHolder, lineWidth: 1.4) {
-                store.send(.viewEventType(.onSubmit))
-            }
-            .padding(.horizontal, 10)
-            
-            ScrollView {
-                ZStack(alignment: .top) {
-                    ZStack {
-                        LazyVStack {
-                            ForEach(Array(store.videoInfos.enumerated()), id: \.element.self) { index, data in
-                                MoreCharacterListView(setModel: data)
-                                    .asButton { // 선택되었을때
-                                        print(data)
-                                    }
-                                    .onAppear {
-                                        store.send(.viewEventType(.videoOnAppear(index)))
-                                    }
-                                    .padding(.horizontal, 12)
-                                    .padding(.bottom, 10)
+        ScrollView {
+            ZStack(alignment: .top) {
+                VStack {}
+                .background {
+                    ScrollDetector { offset in
+                        let offsetChange = offset - lastScrollOffset
+                        
+                        if abs(offsetChange) > 30 {
+                            if offsetChange > 0 {
+                                withAnimation {
+                                    isSearchVisible = false
+                                }
+                            } else if offsetChange < 0 || offset < -50 {
+                                withAnimation {
+                                    isSearchVisible = true
+                                }
+                            }
+                            print(offset)
+                            print(isSearchVisible)
+                        }
+                        else if offset < 20 {
+                            withAnimation {
+                                isSearchVisible = true
                             }
                         }
-                        .padding(.top, 110)
-                    }
-                    
-                    ZStack {
-                        wantMoreInfoView()
-                            .padding(.top, 5)
-                            .padding(.vertical, 4)
-                    }
-                    .zIndex(100)
+                        
+                        lastScrollOffset = offset
+                        scrollOffset = offset
+                    } onDraggingEnd: { offset, veloc in }
                 }
                 
+                ZStack {
+                    Group {
+                        if store.state.videoInfos.isEmpty {
+                            ProgressView()
+                        } else {
+                            listContentView()
+                        }
+                    }
+                    .padding(.top, 130)
+                }
+                ZStack {
+                    wantMoreInfoView()
+                        .padding(.top, 20)
+                        .padding(.vertical, 4)
+                }
+                .zIndex(100)
             }
-            .background(Color(GuideUColor.ViewBaseColor.light.backColor))
+            
         }
+        .background(Color(GuideUColor.ViewBaseColor.light.backColor))
+    }
+    
+    private func listContentView() -> some View {
+        LazyVStack {
+            ForEach(Array(store.videoInfos.enumerated()), id: \.element.self) { index, data in
+                MoreCharacterListView(setModel: data)
+                    .asButton { // 선택되었을때
+                        print(data)
+                    }
+                    .onAppear {
+                        store.send(.viewEventType(.videoOnAppear(index)))
+                    }
+                    .padding(.bottom, 10)
+            }
+        }
+        .padding(.horizontal, 12)
     }
     
     private func wantMoreInfoView() -> some View {
