11e6a99225417eed891c0b42616cfd19868e449f
Kim JaeHyung <you3192001@icloud.com>
2024-08-30 15:04:35 +0900

    [Fetch] xcodeproj

---
+++ GuideU/Presentation/Tab/MoreCharacterView/MoreCharacterListView.swift
@@ -0,0 +1,26 @@
+//
+//  MoreCharacterListView.swift
+//  GuideU
+//
+//  Created by Jae hyung Kim on 8/30/24.
+//
+
+import SwiftUI
+import Alamofire
+
+struct MoreCharacterListView: View {
+    
+    
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+
+#if DEBUG
+#Preview(body: {
+    MoreCharacterListView()
+})
+#endif
