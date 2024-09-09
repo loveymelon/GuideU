2d9d4335192cc8922de32b29fab9f1f67ee53c85
Kim JaeHyung <you3192001@icloud.com>
2024-09-09 20:21:12 +0900

    [UPDATE] UI 변경 커스텀 세그먼트 적용

--- GuideU/Presentation/Tab/MorePerson/MorePersonView.swift
+++ GuideU/Presentation/Tab/MorePerson/MorePersonView.swift
@@ -12,101 +12,98 @@ struct MorePersonView: View {
     
     @State private var offsetY: CGFloat = 0
     @Perception.Bindable var store: StoreOf<PersonFeature>
+    @State private var currentMoreType = moreType.characters
+    @State private var opacity: CGFloat = 1
+    @Namespace var name
+    
+    enum moreType: CaseIterable {
+        case characters
+        case memes
+        
+        var text: String {
+            switch self {
+            case .characters:
+                return "등장인물"
+            case .memes:
+                return "사용되는 밈"
+            }
+        }
+    }
     
     var body: some View {
+        ZStack(alignment: .top) {
+            // 배경 이미지가 네비게이션 바까지 침범하도록 ZStack에 위치
+            backgroundImage(size: CGSize(width: UIScreen.main.bounds.width, height: 300 + -offsetY), minY: offsetY)
+                .ignoresSafeArea(edges: .top) // Safe area까지 무시
+
+            WithPerceptionTracking {
+                VStack(spacing: 0) {
+                    mainView()
+                        .safeAreaInset(edge: .top) {
+                            // 네비게이션 바를 고정시킴
+                            fakeNavigation(entity: store.headerState, opacity: 1 - opacity)
+                                .frame(maxWidth: .infinity)
+                        }
+                }
+                .onAppear {
+                    store.send(.viewCycleType(.onAppear))
+                }
+            }
+        }
+    }
+    
+    private func mainView() -> some View {
         WithPerceptionTracking {
-            VStack(spacing: 0) {
-                GeometryReader {
-                    let safeArea = $0.safeAreaInsets
-                    mainView(safeArea: safeArea)
-                    .ignoresSafeArea(.all, edges: .top)
-                }
-            }
-            .onAppear {
-                store.send(.viewCycleType(.onAppear))
-            }
-        }
-    }
-    
-    private func mainView(safeArea: EdgeInsets) -> some View {
-        
-        GeometryReader { geometry in
-            WithPerceptionTracking {
-                
-                let size = geometry.size
-                
-                ScrollView(.vertical, showsIndicators: false) {
-                    VStack(spacing: 0) {
-                        headerView(size: size, safeArea: safeArea)
-                            .zIndex(1000)
-                        LazyVStack {
-                            ForEach(1...100, id: \.self ) { num in
-                                PersonSectionView()
-                            }
-                            .frame(width: size.width)
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(spacing: 0) {
+                    headerView()
+                        .frame(height: 140)
+                        .background(Color.clear)
+                        .ignoresSafeArea(edges: .top)
+
+                    LazyVStack {
+                        ForEach(1...100, id: \.self ) { num in
+                            PersonSectionView()
                         }
                     }
-                    .background {
-                        ScrollDetector { offset in
-                            offsetY = -offset
-                        } onDraggingEnd: { offset, veloc in
-                            
-                        }
+                    .background(.white)
+                }
+                .background {
+                    ScrollDetector { offset in
+                        offsetY = offset + 80
+                        opacity = max(0, min(1, 1 - (offsetY / 100)))
+                        print("투명도 : ",opacity)
+                    } onDraggingEnd: { offset, veloc in
+                        
                     }
-                    .zIndex(1000)
-                }
-                .toolbar(.hidden, for: .navigationBar)
-            }
-        }
-    }
-    
-    @ViewBuilder
-    func headerView(size: CGSize, safeArea: EdgeInsets) -> some View {
-        let headerHeight = (size.height * 0.3) + safeArea.top
-        let minHeight = 80 + safeArea.top
-        let changeHeight = (headerHeight + offsetY)
-        let opacity = 0.8 + (offsetY / 100)
-        
-        GeometryReader { proxy in
-            WithPerceptionTracking {
-                let size = proxy.size
-                backgroundImage(size: size, minY: offsetY)
-                    .offset(y: -offsetY)
-                    .frame(height: changeHeight < minHeight ? minHeight : changeHeight, alignment: .bottom)
-                
-                VStack(spacing: 20) {
-                    fakeNavigation(size: size, entity: store.headerState, opacity: -opacity)
-                        .padding(.top, safeArea.top + 10)
-                        .offset(y: -offsetY)
-                        .zIndex(3000)
-                    headerContentView(size: size, entity: store.headerState)
-                        .opacity(opacity)
                 }
             }
+            .toolbar(.hidden, for: .navigationBar)
+            
         }
-        .frame(height: headerHeight)
     }
     
-    private func backgroundImage(size: CGSize, minY: CGFloat) -> some View {
-        Group {
-            /// 조건에 따라 이미지 변경할것.
-            Image.defaultBack.resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: size.width, height: size.height + (minY > 0 ? minY : 0))
-                .clipped()
+    func headerView() -> some View {
+        let newHeight: CGFloat = 140
+        return ZStack(alignment: .top) {
+            // 배경 이미지나 콘텐츠
+            VStack {
+                Spacer()
+                headerContentView(entity: store.headerState)
+                    .opacity(opacity)
+            }
+            .background(.clear)
         }
-        .blur(radius: 40, opaque: true)
-        .opacity(0.2)
-        .background(.white)
+        .frame(height: newHeight)
     }
     
-    private func fakeNavigation(size: CGSize, entity: HeaderEntity, opacity: CGFloat)  -> some View {
+    private func fakeNavigation(entity: HeaderEntity, opacity: CGFloat) -> some View {
         ZStack {
             HStack {
                 Image.appLogo
                     .resizable()
                     .aspectRatio(1, contentMode: .fit)
-                    .frame(height: 52)
+                    .frame(height: 42)
                     
                 Text(entity.title)
                     .font(Font(WantedFont.boldFont.font(size: 23)))
@@ -118,15 +115,17 @@ struct MorePersonView: View {
                     Image.search
                         .resizable()
                         .aspectRatio(1, contentMode: .fit)
-                        .frame(height: 32)
+                        .frame(height: 28)
                 }
                 .frame(width: 52)
             }
         }
         .padding(.leading, 10)
+        .frame(height: 50)
+        .background(.white.opacity(opacity))
     }
     
-    private func headerContentView(size: CGSize, entity: HeaderEntity) -> some View {
+    private func headerContentView(entity: HeaderEntity) -> some View {
         VStack(alignment: .leading, spacing: 6) {
             Text(entity.sectionTitle)
                 .font(Font(WantedFont.midFont.font(size: 16)))
@@ -144,8 +143,50 @@ struct MorePersonView: View {
                 Spacer()
             }
             .font(Font(WantedFont.midFont.font(size: 16)))
+            
+            switchView()
         }
         .padding(.horizontal, 10)
+    }
+    
+    private func switchView() -> some View {
+        HStack(spacing: 0) {
+            ForEach(moreType.allCases, id: \.self) { caseOf in
+                VStack {
+                    Text(caseOf.text)
+                        .foregroundStyle(.black)
+                        .asButton {
+                            withAnimation {
+                                currentMoreType = caseOf
+                            }
+                        }
+                    if caseOf == currentMoreType {
+                        Capsule()
+                            .frame(height: 3)
+                            .foregroundColor(Color(GuideUColor.ViewBaseColor.light.primary)) // Capsule 색상 설정
+                            .matchedGeometryEffect(id: "Tab", in: name)
+                    } else {
+                        Capsule()
+                            .frame(height: 3)
+                            .foregroundColor(.clear)
+                    }
+                }
+            }
+        }
+        .frame(height: 50)
+    }
+    
+    private func backgroundImage(size: CGSize, minY: CGFloat) -> some View {
+        Group {
+            /// 조건에 따라 이미지 변경할것.
+            Image.defaultBack.resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: size.width, height: size.height + (minY > 0 ? minY : 0))
+                .clipped()
+        }
+        .blur(radius: 40, opaque: true)
+        .opacity(0.2)
+        .background(.white)
     }
 }
 
