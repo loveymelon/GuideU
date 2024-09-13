ee28a09ed29739dd1bcaba2d78628ef67b0ffb1b
loveyMelon <manciepmc@gmail.com>
2024-09-13 17:02:38 +0900

    [FETCH] ErrorManager Setting And NetworkMoniter Setting

--- GuideU/Presentation/Coordinator/RootCoordinator.swift
+++ GuideU/Presentation/Coordinator/RootCoordinator.swift
@@ -16,28 +16,63 @@ enum RootScreen {
 }
 
 @Reducer
-struct RootCoordinator {
+struct RootCoordinator: Reducer {
     
     @ObservableState
     struct State {
         static let initialState = State(routes: [.root(.splash(SplashFeature.State()), embedInNavigationView: true)])
         
         var routes: IdentifiedArrayOf<Route<RootScreen.State>>
-        
+        var onAppearTrigger: Bool = true
         var viewState: RootCoordinatorViewState = .start
         var tabNavCoordinator: TabNavCoordinator.State = TabNavCoordinator.State.initialState
+        
+        var currentNetworkState = true
+        var alertMessage: AlertMessage? = nil
+    }
+    
+    struct AlertMessage: Identifiable {
+        let id = UUID()
+        var title = ""
+        var actionTitle = ""
+        var cancelTitle = ""
+        var message: String = ""
     }
     
     enum Action {
         case router(IdentifiedRouterActionOf<RootScreen>)
         
         case tabNavCoordinatorAction(TabNavCoordinator.Action)
+        case viewCycle(ViewCycle)
+        case checkError(Bool)
+        case networkError(String)
+        case networkErrorType(NetworkErrorType)
+        
+        // binding
+        case bindingMessage(AlertMessage?)
     }
     
+    enum NetworkErrorType {
+        case nwMonitor
+        case timeOut
+    }
+    
+    enum ViewCycle {
+        case onAppear
+    }
+
     enum RootCoordinatorViewState: Equatable {
         case start
         case tab
     }
+    
+    enum CancelID {
+        case nwMonitor
+        case networkManager
+    }
+    
+    @Dependency(\.nwPathMonitorManager) var nwPathMonitorManager
+    @Dependency(\.networkManager) var networkManager
     
     var body: some ReducerOf<Self> {
         Scope(state: \.tabNavCoordinator, action: \.tabNavCoordinatorAction) {
@@ -47,6 +82,54 @@ struct RootCoordinator {
         Reduce { state, action in
             switch action {
                 
+            case .viewCycle(.onAppear):
+                if state.onAppearTrigger {
+                    state.onAppearTrigger = false
+                    return .run { send in
+                        await send(.networkErrorType(.nwMonitor))
+                        await send(.networkErrorType(.timeOut))
+                    }
+                }
+            
+            case .networkErrorType(.nwMonitor):
+                nwPathMonitorManager.start()
+                
+                return Effect.publisher {
+                    return nwPathMonitorManager.currentConnectionTrigger
+                        .receive(on: RunLoop.main)
+                        .removeDuplicates()
+                }
+                .map { isValid -> Action in
+                    return .checkError(isValid)
+                }
+                .debounce(id: CancelID.nwMonitor, for: 1, scheduler: RunLoop.main)
+                
+            case .networkErrorType(.timeOut):
+                return Effect.publisher {
+                    return networkManager.networkError
+                        .receive(on: RunLoop.main)
+                }
+                .map { isValid -> Action in
+                    return .networkError("네트워크 시간 초과입니다 잠시후에 다시 시작해주세요")
+                }
+                .debounce(id: CancelID.networkManager, for: 1, scheduler: RunLoop.main)
+                
+            case let .checkError(isValid):
+                if state.currentNetworkState != isValid {
+                    state.currentNetworkState = isValid
+                    if !isValid {
+                        return .run { send in
+                            await send(.networkError("네트워크 연결 상태가 좋지 않습니다. 네트워크 상태를 체크해주세요."))
+                        }
+                    } else {
+                        state.alertMessage = nil
+                    }
+                }
+        
+            case let .networkError(networkMessage):
+                state.alertMessage = AlertMessage(message: networkMessage)
+                print(state.alertMessage?.message)
+                
             case let .router(.routeAction(id: _, action: .splash(.delegate(.isFirstUser(trigger))))):
 
                 if trigger {
@@ -54,6 +137,9 @@ struct RootCoordinator {
                 } else {
                     state.viewState = .tab
                 }
+                
+            case let .bindingMessage(alert):
+                state.alertMessage = alert
                 
             default:
                 break
