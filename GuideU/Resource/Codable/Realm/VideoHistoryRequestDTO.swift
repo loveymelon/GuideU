007b97e5e60e4a4e5b8e23041ac9f4463746f36e
loveyMelon <manciepmc@gmail.com>
2024-09-13 20:57:19 +0900

    [Update] realm channelImage 추가 대응

--- GuideU/Resource/Codable/Realm/VideoHistoryRequestDTO.swift
+++ GuideU/Resource/Codable/Realm/VideoHistoryRequestDTO.swift
@@ -14,17 +14,19 @@ class VideoHistoryRequestDTO: Object {
     @Persisted var title: String
     @Persisted var videoURL: String
     @Persisted var channelName: String
+    @Persisted var channelImage: String
     @Persisted var thumbnail: String
     @Persisted var updatedAt: Date
     @Persisted var watchedAt: Date
     
-    convenience init(identifier: String, title: String, videoURL: String, channelName: String, thumbnail: String, updatedAt: Date, watchedAt: Date) {
+    convenience init(identifier: String, title: String, videoURL: String, channelName: String, channelImage: String, thumbnail: String, updatedAt: Date, watchedAt: Date) {
         self.init()
         
         self.identifier = identifier
         self.title = title
         self.videoURL = videoURL
         self.channelName = channelName
+        self.channelImage = channelImage
         self.thumbnail = thumbnail
         self.updatedAt = updatedAt
         self.watchedAt = watchedAt
